defc5b3d7295d79d185fa153b44de79d57b41e04
macbook <macbook@macbooks-MacBook-Air.local>
2024-02-22 21:07:42 +0900

    first test

---
+++ src/main.c
@@ -0,0 +1,19 @@
+#include "minishell.h"
+
+int	main(void)
+{
+	char *line;
+
+	rl_outstream = stderr;
+	while (1)
+	{
+		line = readline("minishell$ ");
+		if (line == NULL)
+			break ;
+		if (*line)
+			add_history(line);
+		// TODO: intepret line as a command
+		free(line);
+	}
+	exit(0);
+}
