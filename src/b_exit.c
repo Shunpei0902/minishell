1a9d28708078e2f64a5d9dcffec72ef39550125b
niida <54788301+naokiiida@users.noreply.github.com>
2025-01-12 01:44:24 +0900

    fix compatibility with LONG overflow and underflow status

--- src/b_exit.c
+++ src/b_exit.c
@@ -6,27 +6,57 @@
 /*   By: niida <niida@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/09 17:34:19 by niida             #+#    #+#             */
-/*   Updated: 2024/12/17 13:51:41 by niida            ###   ########.fr       */
+/*   Updated: 2025/01/12 00:59:14 by niida            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include "minishell.h"
 
-bool	isnum(const char *str)
+int	ft_strtol(const char *str)
 {
-	size_t	i;
+	long long	ans;
+	long		abs;
 
-	i = 0;
+	ans = 0;
+	abs = 1;
 	while (*str == ' ' || (*str >= '\t' && *str <= '\r'))
 		str++;
 	if (*str == '-' || *str == '+')
+	{
+		if (*str == '-')
+			abs = -1;
 		str++;
-	while (ft_isdigit(*str))
+	}
+	while (*str >= '0' && *str <= '9')
+	{
+		if (abs == 1 && (ans > LONG_MAX / 10
+				|| (ans == LONG_MAX / 10 && (*str - '0') > LONG_MAX % 10)))
+			return (2);
+		else if (abs == -1 && (-ans < LONG_MIN / 10
+				|| (ans == -(LONG_MIN / 10) && (*str - '0') > -(LONG_MIN % 10))))
+			return (2);
+		ans = ans * 10 + (*str - '0');
 		str++;
-	while (*str == ' ' || (*str >= '\t' && *str <= '\r'))
+	}
+	return ((int)(abs * ans));
+}
+
+bool	isnum(const char *str)
+{
+	if (!str || *str == '\0')
+		return (false);
+	if (*str == '-' || *str == '+')
 		str++;
-	return (str[i] == '\0');
+	if (*str == '\0')
+		return (false);
+	while (*str)
+	{
+		if (!ft_isdigit(*str))
+			return (false);
+		str++;
+	}
+	return (true);
 }
 
 // TODO: on shell exit, pass status to g_last_status and hashmap_destroy();
@@ -44,12 +74,12 @@ int	b_exit(char **av)
 		status = g_last_status;
 	else if (av[2] != NULL)
 	{
-		ft_putstr_fd("exit: too many arguments", STDOUT_FILENO);
+		ft_putstr_fd("minishell: exit: too many arguments\n", STDOUT_FILENO);
 		return (1);
 	}
 	else if (!isnum(av[1]))
 		err_exit("exit", "numeric argument required", 2);
 	else
-		status = (uint8_t)ft_atoi(av[1]);
-	exit (status);
+		status = (uint8_t)ft_strtol(av[1]);
+	exit(status);
 }
