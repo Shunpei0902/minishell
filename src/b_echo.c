efdeccaee010c1a18faa6c0608ef15ffdc0aec72
niida <54788301+naokiiida@users.noreply.github.com>
2025-01-12 01:43:48 +0900

    compatible with echo -nnnn as an option, while echo -n-n-n is a string

--- src/b_echo.c
+++ src/b_echo.c
@@ -6,29 +6,41 @@
 /*   By: niida <niida@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/09 17:33:54 by niida             #+#    #+#             */
-/*   Updated: 2024/12/17 13:51:41 by niida            ###   ########.fr       */
+/*   Updated: 2025/01/12 01:29:34 by niida            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include "minishell.h"
 
+static bool	check_option_n(char **av)
+{
+	char	*str;
+
+	if (!av || !*av || **av != '-')
+		return (true);
+	str = *av + 1;
+	while (*str == 'n')
+		str++;
+	if (*str)
+		return (true);
+	return (false);
+}
+
 int	b_echo(char **av)
 {
 	bool	op_newline;
 
-	op_newline = true;
-	if (*++av && ft_strncmp("-n", *av, 2) == 0)
-	{
-		op_newline = false;
-		++av;
-	}
+	av++;
+	op_newline = check_option_n(av);
+	if (!op_newline)
+		av++;
 	while (*av)
 	{
 		write(1, *av, strlen(*av));
 		if (*(av + 1))
 			write(1, " ", 1);
-		++av;
+		av++;
 	}
 	if (op_newline)
 		write(1, "\n", 1);
