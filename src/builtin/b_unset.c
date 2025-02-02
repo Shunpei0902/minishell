033fcde869e29e7358b04668fdcf1ed429fd0a86
macbook <macbook@macbooks-MacBook-Air.local>
2025-02-02 10:19:40 +0900

    fix error_massage

--- src/builtin/b_unset.c
+++ src/builtin/b_unset.c
@@ -6,7 +6,7 @@
 /*   By: sasano <shunkotkg0141@gmail.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/13 16:50:42 by niida             #+#    #+#             */
-/*   Updated: 2025/01/24 18:05:09 by sasano           ###   ########.fr       */
+/*   Updated: 2025/02/02 10:18:57 by sasano           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ int	b_unset(char **av)
 	{
 		if (hash_unset(*av))
 		{
-			error_message("unset: ", "not a valid identifier");
+			error_message3("unset: ", "not a valid identifier", NULL);
 			status = 1;
 		}
 		else
