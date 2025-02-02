d4e777239e8934bc2e4231bb9de85c94d8ed169b
macbook <macbook@macbooks-MacBook-Air.local>
2025-02-02 21:20:07 +0900

    fix calloc

--- src/xlib/xcalloc.c
+++ src/xlib/xcalloc.c
@@ -6,7 +6,7 @@
 /*   By: sasano <shunkotkg0141@gmail.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/23 13:36:01 by sasano            #+#    #+#             */
-/*   Updated: 2025/01/23 19:12:40 by sasano           ###   ########.fr       */
+/*   Updated: 2025/02/02 21:19:44 by sasano           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void	*xcalloc(size_t count, size_t size)
 {
 	void	*ptr;
 
-	ptr = calloc(count, size);
+	ptr = ft_calloc(count, size);
 	if (!ptr)
 		fatal_error("calloc failed");
 	return (ptr);
